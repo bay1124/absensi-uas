0128626b3166b60425b18e45839cb29de62be5b0
bay1124 <bayupraetyobayu24@gmail.com>
2024-01-03 12:27:13 +0700

    first commit

---
+++ Absensi_mahasiswa.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, Mata_Kuliah, Tanggal_Perkuliahan, Nama, NIM, Jurusan):
+        self.Mata_Kuliah = Mata_Kuliah
+        self.Tanggal_Perkuliahan = Tanggal_Perkuliahan
+        self.Nama = Nama
+        self.NIM = NIM
+        self.Jurusan = Jurusan
